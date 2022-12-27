647058f14693effcd359f62a66c1b56e03ebf3b8
luvlore <brunaluchini22@gmail.com>
2022-12-27 19:55:33 -0300

    :sparkles: feat: adiciona cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,15 @@
+import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 import router from './routes';
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(router);
+app.use(cors);
 
-app.listen(port, () => {
-    console.log(`Server is up and running at: http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is up and running at: http://localhost:${PORT}`);
 });
